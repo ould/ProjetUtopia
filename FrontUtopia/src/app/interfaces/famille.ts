6547cb243af78786184762a76b4e8428f5e35d61
hellald <hellald@LAPTOP-1C7Z8HOR>
2023-11-01 00:07:57 +0100

    Ajout famille

--- FrontUtopia/src/app/interfaces/famille.ts
+++ FrontUtopia/src/app/interfaces/famille.ts
@@ -3,10 +3,10 @@ import { Personne } from "src/app/interfaces/Personne";
 export interface Famille {
     id: number;
     nomFamille: string;
-    personnes : Personne[];
-    composition: string;
-    nationalite : string;
-    commentaire: string;
+    personnes: Personne[];
+    composition?: string;
+    nationalite?: string;
+    commentaire?: string;
 }
 
 
